48f30be17e107efc6a90bd847e449e04388bd5f9
fernandes <buidinhcanh2006@gmail.com>
2025-02-17 23:08:24 +0700

    I owned git

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <variant>  // C++17 feature
+using namespace std;
+
 
 int main() {
     std::variant<int, std::string> data = "C++17 is enabled!";
     std::cout << std::get<std::string>(data) << std::endl;
+    cout << "cuối cùng t đã biết dùng git" << endl;
     return 0;
 }
