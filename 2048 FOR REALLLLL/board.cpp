09303ee4eb164cc33a9243cb781dd34438a5e751
fernandes <buidinhcanh2006@gmail.com>
2025-03-09 17:44:42 +0700

    modìied 01

--- 2048 FOR REALLLLL/board.cpp
+++ 2048 FOR REALLLLL/board.cpp
@@ -1,9 +1,33 @@
+// sinh ô ngẫu nhiên , tạo bảng , di chuyển , kiểm tra thắng thua .
 #include <iostream>
 #include "board.h"
 #include <vector>
-
-board::board(int s) : size(s), net(s, std::vector<int>(s, 0))
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+Board::Board(int size) : size(size), net(size, std::vector<int>(size, 0))
 {
-    addNewTile();
-    addNewTile();
+    srand(time(0));
+    add_randomTile();
+    add_randomTile(); // 2 lần xuất hiện của 2 ô ngẫu nhiên đầu game
+}
+// vẽ bảng
+void Board ::drawboard() const
+{
+    system("clear");
+    for (const vector<int> &row : net) // thay vì in ra số 0 của ô thì in ra .
+    {
+        for (int cell : row)
+        {
+            if (cel == 0)
+            {
+                cout << ".\t";
+            }
+            else
+            {
+                cout << cell << "\t";
+            }
+        }
+        cout << "\n";
+    }
 }
