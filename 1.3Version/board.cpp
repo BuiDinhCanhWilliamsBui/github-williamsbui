624f7eaa5ebe91c77cb31039b58b5a58b697bd03
fernandes <buidinhcanh2006@gmail.com>
2025-05-03 23:54:18 +0700

    okay

--- 1.3Version/board.cpp
+++ 1.3Version/board.cpp
@@ -1,3 +1,171 @@
+// #include <iostream>
+// #include "board.h"
+// #include <vector>
+// #include <cstdlib>
+// #include <ctime>
+// #include <iomanip>
+// #include <string>
+// #include <sstream>
+// using namespace std;
+
+// Board::Board(int size) : size(size), net(size, std::vector<int>(size, 0))
+// {
+//     srand(time(0));
+//     add_randomTile();
+//     add_randomTile();
+// }
+
+// void Board::add_randomTile()
+// {
+//     vector<pair<int, int>> check;
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             if (net[i][j] == 0)
+//             {
+//                 check.push_back({i, j});
+//             }
+//         }
+//     }
+//     if (!check.empty())
+//     {
+//         std::pair<int, int> cell = check[rand() % check.size()];
+//         int a = cell.first, b = cell.second;
+//         net[a][b] = (rand() % 2 == 0) ? 2 : 4;
+//     }
+// }
+
+// bool Board::check_Win() const
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             if (net[i][j] == 2048)
+//             {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// bool Board::check_Gameover() const
+// {
+//     for (const auto &row : net)
+//     {
+//         for (int cell : row)
+//         {
+//             if (cell == 0)
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             if ((i < size - 1 && net[i][j] == net[i + 1][j]) ||
+//                 (j < size - 1 && net[i][j] == net[i][j + 1]))
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+// SDL_Color getColorForValue(int value)
+// {
+//     switch (value)
+//     {
+//     case 0:
+//         return {205, 193, 180, 255};
+//     case 2:
+//         return {238, 228, 218, 255};
+//     case 4:
+//         return {237, 224, 200, 255};
+//     case 8:
+//         return {242, 177, 121, 255};
+//     case 16:
+//         return {245, 149, 99, 255};
+//     case 32:
+//         return {246, 124, 95, 255};
+//     case 64:
+//         return {246, 94, 59, 255};
+//     case 128:
+//         return {237, 207, 114, 255};
+//     case 256:
+//         return {237, 204, 97, 255};
+//     case 512:
+//         return {237, 200, 80, 255};
+//     case 1024:
+//         return {237, 197, 63, 255};
+//     case 2048:
+//         return {237, 194, 46, 255};
+//     default:
+//         return {60, 58, 50, 255};
+//     }
+// }
+
+// void Board::drawBoardSDL(SDL_Renderer *renderer, TTF_Font *font, int windowWidth, int windowHeight) const
+// {
+//     int size = this->size;
+//     int margin = 10;
+//     int boardSize = std::min(windowWidth, windowHeight) - 2 * margin;
+//     int tileSize = (boardSize - (size + 1) * margin) / size;
+
+//     SDL_Color textColor = {119, 110, 101, 255};
+
+//     // Vẽ background bảng
+//     SDL_SetRenderDrawColor(renderer, 187, 173, 160, 255);
+//     SDL_Rect boardRect = {margin, margin, boardSize, boardSize};
+//     SDL_RenderFillRect(renderer, &boardRect);
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         for (int j = 0; j < size; j++)
+//         {
+//             int val = net[i][j];
+//             SDL_Color tileColor = getColorForValue(val);
+//             SDL_SetRenderDrawColor(renderer, tileColor.r, tileColor.g, tileColor.b, tileColor.a);
+
+//             SDL_Rect tileRect = {
+//                 margin + (tileSize + margin) * j + margin,
+//                 margin + (tileSize + margin) * i + margin,
+//                 tileSize,
+//                 tileSize};
+
+//             SDL_RenderFillRect(renderer, &tileRect);
+
+//             if (val != 0)
+//             {
+//                 std::stringstream ss;
+//                 ss << val;
+//                 std::string text = ss.str();
+
+//                 SDL_Surface *surface = TTF_RenderText_Blended(font, text.c_str(), textColor);
+//                 SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+//                 int textW = surface->w;
+//                 int textH = surface->h;
+
+//                 SDL_FreeSurface(surface);
+
+//                 SDL_Rect dstRect = {
+//                     tileRect.x + (tileSize - textW) / 2,
+//                     tileRect.y + (tileSize - textH) / 2,
+//                     textW,
+//                     textH};
+
+//                 SDL_RenderCopy(renderer, texture, nullptr, &dstRect);
+//                 SDL_DestroyTexture(texture);
+//             }
+//         }
+//     }
+// }
 #include <iostream>
 #include "board.h"
 #include <vector>
@@ -114,14 +282,21 @@ void Board::drawBoardSDL(SDL_Renderer *renderer, TTF_Font *font, int windowWidth
 {
     int size = this->size;
     int margin = 10;
-    int boardSize = std::min(windowWidth, windowHeight) - 2 * margin;
+    // Tính kích thước bảng sao cho vừa với cửa sổ và căn giữa
+    int availableWidth = windowWidth - 2 * margin;
+    int availableHeight = windowHeight - 2 * margin;
+    int boardSize = std::min(availableWidth, availableHeight);
     int tileSize = (boardSize - (size + 1) * margin) / size;
+
+    // Căn giữa bảng
+    int boardX = (windowWidth - boardSize) / 2;
+    int boardY = (windowHeight - boardSize) / 2;
 
     SDL_Color textColor = {119, 110, 101, 255};
 
     // Vẽ background bảng
     SDL_SetRenderDrawColor(renderer, 187, 173, 160, 255);
-    SDL_Rect boardRect = {margin, margin, boardSize, boardSize};
+    SDL_Rect boardRect = {boardX, boardY, boardSize, boardSize};
     SDL_RenderFillRect(renderer, &boardRect);
 
     for (int i = 0; i < size; i++)
@@ -133,8 +308,8 @@ void Board::drawBoardSDL(SDL_Renderer *renderer, TTF_Font *font, int windowWidth
             SDL_SetRenderDrawColor(renderer, tileColor.r, tileColor.g, tileColor.b, tileColor.a);
 
             SDL_Rect tileRect = {
-                margin + (tileSize + margin) * j + margin,
-                margin + (tileSize + margin) * i + margin,
+                boardX + margin + (tileSize + margin) * j,
+                boardY + margin + (tileSize + margin) * i,
                 tileSize,
                 tileSize};
 
@@ -146,7 +321,16 @@ void Board::drawBoardSDL(SDL_Renderer *renderer, TTF_Font *font, int windowWidth
                 ss << val;
                 std::string text = ss.str();
 
+                // Điều chỉnh kích thước font dựa trên kích thước ô
+                int fontSize = tileSize / 3;
+                TTF_SetFontSize(font, fontSize);
+
                 SDL_Surface *surface = TTF_RenderText_Blended(font, text.c_str(), textColor);
+                if (!surface)
+                {
+                    std::cerr << "TTF_RenderText_Blended error: " << TTF_GetError() << std::endl;
+                    continue;
+                }
                 SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
                 int textW = surface->w;
--- 1.3Version/sdlmain.cpp
+++ 1.3Version/sdlmain.cpp
@@ -1,6 +1,204 @@
+// #include <SDL2/SDL.h>
+// #include <SDL2/SDL_ttf.h>
+// #include <iostream>
+// #include "game.h"
 
+// const int WINDOW_WIDTH = 600;
+// const int WINDOW_HEIGHT = 700;
+
+// void drawText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y, SDL_Color color)
+// {
+//     SDL_Surface *surface = TTF_RenderText_Blended(font, text, color);
+//     if (!surface)
+//     {
+//         std::cerr << "TTF_RenderText_Blended error: " << TTF_GetError() << std::endl;
+//         return;
+//     }
+//     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+//     SDL_Rect dstRect = {x, y, surface->w, surface->h};
+//     SDL_FreeSurface(surface);
+//     SDL_RenderCopy(renderer, texture, nullptr, &dstRect);
+//     SDL_DestroyTexture(texture);
+// }
+
+// void renderGameOver(SDL_Renderer *renderer, TTF_Font *font)
+// {
+//     // Vẽ nền mờ
+//     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+//     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 180);
+//     SDL_Rect overlay = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+//     SDL_RenderFillRect(renderer, &overlay);
+
+//     // Tăng kích thước font cho chữ "Game Over!"
+//     TTF_SetFontSize(font, 48);
+//     SDL_Color yellow = {255, 255, 0, 255};
+//     drawText(renderer, font, "Game Over!", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 60, yellow);
+
+//     // Font nhỏ hơn cho "Press R to Restart"
+//     TTF_SetFontSize(font, 36);
+//     SDL_Color white = {255, 255, 255, 255};
+//     drawText(renderer, font, "Press R to Restart", WINDOW_WIDTH / 2 - 120, WINDOW_HEIGHT / 2 + 20, white);
+// }
+
+// int main(int argc, char *argv[])
+// {
+//     if (SDL_Init(SDL_INIT_VIDEO) != 0)
+//     {
+//         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+//         return 1;
+//     }
+//     if (TTF_Init() != 0)
+//     {
+//         std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+//         SDL_Quit();
+//         return 1;
+//     }
+
+//     SDL_Window *window = SDL_CreateWindow("2048 Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+//                                           WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+//     if (!window)
+//     {
+//         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+//         TTF_Quit();
+//         SDL_Quit();
+//         return 1;
+//     }
+
+//     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+//     if (!renderer)
+//     {
+//         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+//         SDL_DestroyWindow(window);
+//         TTF_Quit();
+//         SDL_Quit();
+//         return 1;
+//     }
+
+//     TTF_Font *font = TTF_OpenFont("Arial.ttf", 36);
+//     if (!font)
+//     {
+//         std::cerr << "TTF_OpenFont Error: " << TTF_GetError() << std::endl;
+//         SDL_DestroyRenderer(renderer);
+//         SDL_DestroyWindow(window);
+//         TTF_Quit();
+//         SDL_Quit();
+//         return 1;
+//     }
+
+//     Game game(4);
+//     bool quit = false;
+//     bool gameStarted = false;
+//     bool gameWon = false;
+//     bool gameOver = false;
+//     SDL_Event event;
+
+//     while (!quit)
+//     {
+//         while (SDL_PollEvent(&event))
+//         {
+//             if (event.type == SDL_QUIT)
+//             {
+//                 quit = true;
+//             }
+//             if (event.type == SDL_KEYDOWN)
+//             {
+//                 if (!gameStarted)
+//                 {
+//                     if (event.key.keysym.sym == SDLK_RETURN)
+//                     {
+//                         gameStarted = true;
+//                     }
+//                 }
+//                 else if (!gameWon && !gameOver)
+//                 {
+//                     bool moved = false;
+//                     switch (event.key.keysym.sym)
+//                     {
+//                     case SDLK_w:
+//                         moved = game.moveUp();
+//                         break;
+//                     case SDLK_s:
+//                         moved = game.moveDown();
+//                         break;
+//                     case SDLK_a:
+//                         moved = game.moveLeft();
+//                         break;
+//                     case SDLK_d:
+//                         moved = game.moveRight();
+//                         break;
+//                     case SDLK_ESCAPE:
+//                         quit = true;
+//                         break;
+//                     }
+//                     if (moved)
+//                     {
+//                         game.board.add_randomTile();
+//                     }
+//                 }
+//                 else if (event.key.keysym.sym == SDLK_r)
+//                 {
+//                     // Reset game
+//                     game = Game(4);
+//                     gameStarted = true;
+//                     gameWon = false;
+//                     gameOver = false;
+//                 }
+//             }
+//         }
+
+//         SDL_SetRenderDrawColor(renderer, 187, 173, 160, 255);
+//         SDL_RenderClear(renderer);
+
+//         if (!gameStarted)
+//         {
+//             SDL_Color white = {255, 255, 255, 255};
+//             drawText(renderer, font, "2048 Game", WINDOW_WIDTH / 2 - 80, WINDOW_HEIGHT / 2 - 50, white);
+//             drawText(renderer, font, "Press Enter to Start", WINDOW_WIDTH / 2 - 120, WINDOW_HEIGHT / 2 + 10, white);
+//         }
+//         else
+//         {
+//             game.board.drawBoardSDL(renderer, font, WINDOW_WIDTH, WINDOW_HEIGHT - 100);
+
+//             SDL_Color black = {0, 0, 0, 255};
+//             char scoreText[64];
+//             snprintf(scoreText, sizeof(scoreText), "Score: %d", game.score);
+//             drawText(renderer, font, scoreText, 10, WINDOW_HEIGHT - 80, black);
+
+//             if (game.board.check_Win() && !gameWon)
+//             {
+//                 gameWon = true;
+//             }
+//             if (game.board.check_Gameover() && !gameOver)
+//             {
+//                 gameOver = true;
+//             }
+
+//             if (gameWon)
+//             {
+//                 SDL_Color white = {255, 255, 255, 255};
+//                 drawText(renderer, font, "You Win!", WINDOW_WIDTH / 2 - 60, WINDOW_HEIGHT / 2 - 20, white);
+//                 drawText(renderer, font, "Press R to Restart", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 + 20, white);
+//             }
+//             else if (gameOver)
+//             {
+//                 renderGameOver(renderer, font);
+//             }
+//         }
+
+//         SDL_RenderPresent(renderer);
+//         SDL_Delay(16);
+//     }
+
+//     TTF_CloseFont(font);
+//     SDL_DestroyRenderer(renderer);
+//     SDL_DestroyWindow(window);
+//     TTF_Quit();
+//     SDL_Quit();
+//     return 0;
+// }
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include "game.h"
 
@@ -22,25 +220,6 @@ void drawText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, i
     SDL_DestroyTexture(texture);
 }
 
-void renderGameOver(SDL_Renderer *renderer, TTF_Font *font)
-{
-    // Vẽ nền mờ
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 180);
-    SDL_Rect overlay = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
-    SDL_RenderFillRect(renderer, &overlay);
-
-    // Tăng kích thước font cho chữ "Game Over!"
-    TTF_SetFontSize(font, 48);
-    SDL_Color yellow = {255, 255, 0, 255};
-    drawText(renderer, font, "Game Over!", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 60, yellow);
-
-    // Font nhỏ hơn cho "Press R to Restart"
-    TTF_SetFontSize(font, 36);
-    SDL_Color white = {255, 255, 255, 255};
-    drawText(renderer, font, "Press R to Restart", WINDOW_WIDTH / 2 - 120, WINDOW_HEIGHT / 2 + 20, white);
-}
-
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -54,12 +233,20 @@ int main(int argc, char *argv[])
         SDL_Quit();
         return 1;
     }
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+    {
+        std::cerr << "IMG_Init Error: " << IMG_GetError() << std::endl;
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
 
     SDL_Window *window = SDL_CreateWindow("2048 Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window)
     {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return 1;
@@ -70,6 +257,7 @@ int main(int argc, char *argv[])
     {
         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
+        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return 1;
@@ -81,10 +269,43 @@ int main(int argc, char *argv[])
         std::cerr << "TTF_OpenFont Error: " << TTF_GetError() << std::endl;
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
+        IMG_Quit();
         TTF_Quit();
         SDL_Quit();
         return 1;
     }
+
+    // Load hình ảnh cho màn hình bắt đầu và game over
+    SDL_Surface *startSurface = IMG_Load("start_screen.png");
+    if (!startSurface)
+    {
+        std::cerr << "IMG_Load Error (start_screen.png): " << IMG_GetError() << std::endl;
+        TTF_CloseFont(font);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture *startTexture = SDL_CreateTextureFromSurface(renderer, startSurface);
+    SDL_FreeSurface(startSurface);
+
+    SDL_Surface *gameOverSurface = IMG_Load("game_over.png");
+    if (!gameOverSurface)
+    {
+        std::cerr << "IMG_Load Error (game_over.png): " << IMG_GetError() << std::endl;
+        SDL_DestroyTexture(startTexture);
+        TTF_CloseFont(font);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture *gameOverTexture = SDL_CreateTextureFromSurface(renderer, gameOverSurface);
+    SDL_FreeSurface(gameOverSurface);
 
     Game game(4);
     bool quit = false;
@@ -152,9 +373,9 @@ int main(int argc, char *argv[])
 
         if (!gameStarted)
         {
-            SDL_Color white = {255, 255, 255, 255};
-            drawText(renderer, font, "2048 Game", WINDOW_WIDTH / 2 - 80, WINDOW_HEIGHT / 2 - 50, white);
-            drawText(renderer, font, "Press Enter to Start", WINDOW_WIDTH / 2 - 120, WINDOW_HEIGHT / 2 + 10, white);
+            // Hiển thị hình ảnh màn hình bắt đầu
+            SDL_Rect startRect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+            SDL_RenderCopy(renderer, startTexture, nullptr, &startRect);
         }
         else
         {
@@ -182,7 +403,14 @@ int main(int argc, char *argv[])
             }
             else if (gameOver)
             {
-                renderGameOver(renderer, font);
+                // Hiển thị hình ảnh game over
+                SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 180);
+                SDL_Rect overlay = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+                SDL_RenderFillRect(renderer, &overlay);
+
+                SDL_Rect gameOverRect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+                SDL_RenderCopy(renderer, gameOverTexture, nullptr, &gameOverRect);
             }
         }
 
@@ -190,9 +418,12 @@ int main(int argc, char *argv[])
         SDL_Delay(16);
     }
 
+    SDL_DestroyTexture(startTexture);
+    SDL_DestroyTexture(gameOverTexture);
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
     TTF_Quit();
     SDL_Quit();
     return 0;
